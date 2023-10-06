a58b603c4600f30def85497ab31794e1a6e91519
Tripura kant <tripura.kant@zoom.us>
2023-10-06 06:23:57 +0530

    Automated commit at Fri Oct  6 06:23:57 IST 2023

--- 21.go
+++ 21.go
@@ -1,1 +1,20 @@
-# This is file 21.go
+// Go by example -3
+
+package main
+import "fmt"
+
+func main() {
+
+    var a = "initial"
+    fmt.Println(a)
+
+    var b,c int = 1,2
+    fmt.Println(b,c)
+
+    var d = true
+    fmt.Println(d)
+
+    var e int
+    fmt.Println(e)
+}
+
